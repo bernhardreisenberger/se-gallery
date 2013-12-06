f9451b57bce5a23d8673a5f9fbb569706c0b8d0a
Bernhard Reisenberger <bernhard.reisenberger@gmail.com>
2013-12-06 20:11:21 +0100

    single sign on functionality added. (does not work yet ;) )

--- server.js
+++ server.js
@@ -6,7 +6,9 @@
 var express = require('express')
   , routes = require('./routes')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , passport = require('passport')
+  , GoogleStrategy = require('passport-google').Strategy;
 
 var app = express();
 
@@ -18,6 +20,7 @@ app.configure(function () {
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
+    app.use(passport.initialize());
     app.use(app.router);
     app.use(express.static(path.join(__dirname, 'public')));
 });
@@ -26,13 +29,36 @@ app.configure('development', function () {
     app.use(express.errorHandler());
 });
 
+passport.use(new GoogleStrategy({
+    returnURL: 'http://localhost:3000/auth/google/return',
+    realm: 'http://localhost:3000/'
+},
+  function (identifier, done) {
+      console.log('ident:' + identifier);
+      //User.findByOpenID({ openId: identifier }, function (err, user) {
+      //    return done(err, user);
+      //});
+  }
+));
+
+
+
 app.get('/', function (req, res) {
-  res.render('index', {title: 'Home'})
+    res.render('index', { title: 'Home' })
 });
 app.get('/gallery', routes.showall);
 //:filter can be accessed with the req.param()
 app.get('/:filter', routes.filter);
 app.post('/upload', routes.upload);
+app.get('/auth/google',
+  passport.authenticate('google'));
+app.get('/auth/google/return',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function (req, res) {
+      // Successful authentication, redirect home.
+      res.redirect('/');
+  });
+
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log("Express server listening on port " + app.get('port'));
